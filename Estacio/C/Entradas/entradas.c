404242d8507efaad97e6c81fb9e70daab2faebca
Samuel Zanatta <sa.zanatta@hotmail.com>
2025-06-03 20:18:17 -0300

    adicionados programas para o nivel 1 do modulo 1

---
+++ Estacio/C/Entradas/entradas.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+  int idade;
+  char nome[50];
+  float altura;
+
+  printf("Digite a sua idade: \n");
+  scanf("%d", &idade);
+  printf("Idade: %d \n", idade);
+
+
+}
---
+++ Estacio/C/Variaveis/variaveis.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+  int inteiro = 1;
+  char letra = 'C';
+  char palavra[] = "Quanta Frescura pqp";
+  float float1 = 1.85;
+  double double1 = 1.8547;
+
+  printf("Teste Variáveis \n");
+  printf("int: %i\n", inteiro);
+
+  /*
+  
+  %d - Números
+  %i - Igual ao %d
+  %s - String
+  %c - Char
+  %e - Ponto flutuante com Notação Científica
+  %f - Ponto flutuante normal
+  
+  */
+  
+
+}
